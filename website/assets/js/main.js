7b9d3c8f60fdce3aca077cc216e1f204517d5e6f
Luigi Espinosa <numerocuatro@pm.me>
2020-07-01 13:52:22 -0500

    sort-by dropdown

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -1,8 +1,8 @@
-import API from './api.js'
-import Statistic from './statistic.js'
-import Countries from './country.js'
+// import API from './api.js'
+// import Statistic from './statistic.js'
+// import Countries from './country.js'
 
-const api = new API()
+// const api = new API()
 
 const chk = document.getElementById('chk');
 chk.addEventListener('change', () => {
@@ -14,17 +14,37 @@ chk_desktop.addEventListener('change', () => {
 	document.body.classList.toggle('light');
 });
 
-async function renderWorldTotalData() {
-    const data = await api.getWorldTotalData()
-    console.log(data)
-    new Statistic(data)
-}
+const usSort = document.querySelector('.drop-us').addEventListener("click", openUsMenu);
+const jpSort = document.querySelector('.drop-japan').addEventListener("click", openJpMenu);
+const countrySort = document.querySelector('.drop-country').addEventListener("click", openCountryMenu);
 
-async function renderCountryData() {
-    const country = await api.getCountries()
-    console.log(country)
-    new Countries(country)
-}
+function openUsMenu() { document.querySelector('.us-dropdown').classList.toggle('show') }
+function openJpMenu() { document.querySelector('.jp-dropdown').classList.toggle('show') }
+function openCountryMenu() { document.querySelector('.country-dropdown').classList.toggle('show') }
 
-renderWorldTotalData()
-renderCountryData()
+const handleClick = (e) => {
+    e.preventDefault();
+    const active = document.querySelector('.country-dropdown .slots');
+    if(active){
+      active.classList.remove('active');
+    }
+    e.currentTarget.classList.add('active');
+  }
+
+document.querySelector('.country-dropdown .slots').addEventListener("click", handleClick);
+
+
+// async function renderWorldTotalData() {
+//     const data = await api.getWorldTotalData()
+//     console.log(data)
+//     new Statistic(data)
+// }
+
+// async function renderCountryData() {
+//     const country = await api.getCountries()
+//     console.log(country)
+//     new Countries(country)
+// }
+
+// renderWorldTotalData()
+// renderCountryData()
