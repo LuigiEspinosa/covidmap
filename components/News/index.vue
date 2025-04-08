e8861f44a41b50b8e1e84b5e421f1cb9179a220e
Número Cuatro <luigi@cuatro.dev>
2025-04-08 12:34:52 -0500

    fix: News Container Height

--- components/News/index.vue
+++ components/News/index.vue
@@ -1,20 +1,20 @@
 <template>
-  <Box custom="flex-1 overflow-auto min-h-fit">
-    <template #box>
-      <h2 class="font-semibold uppercase text-[#2ab75b] text-[1.6em] my-2">Latest News</h2>
+	<Box custom="flex-1 overflow-auto">
+		<template #box>
+			<h2 class="font-semibold uppercase text-[#2ab75b] text-[1.6em] my-2">Latest News</h2>
 
-      <ul class="mt-3">
-        <li v-for="news in data?.articles" class="mb-2">
-          <nuxt-link :to="news?.url" target="_blank" class="text-black dark:text-white group">
-            <h2 class="group-hover:text-[#2ab75b] text-[1.2em]">
-              {{ news?.title }}
-            </h2>
-            <h3 class="text-[#2ab75b] text-[1em]">{{ formatDate(news?.publishedAt) }}</h3>
-          </nuxt-link>
-        </li>
-      </ul>
-    </template>
-  </Box>
+			<ul class="mt-3 overflow-auto">
+				<li v-for="news in data?.articles" class="mb-2">
+					<nuxt-link :to="news?.url" target="_blank" class="text-black dark:text-white group">
+						<h2 class="group-hover:text-[#2ab75b] text-[1.2em]">
+							{{ news?.title }}
+						</h2>
+						<h3 class="text-[#2ab75b] text-[1em]">{{ formatDate(news?.publishedAt) }}</h3>
+					</nuxt-link>
+				</li>
+			</ul>
+		</template>
+	</Box>
 </template>
 
 <script setup lang="ts">
@@ -24,7 +24,8 @@ import human from "human-time";
 const { data } = await useFetch<News>("/api/news");
 
 const formatDate = (dateString: string | number | Date) => {
-  const date = new Date(dateString);
-  return human(date);
+	const date = new Date(dateString);
+	return human(date);
 };
 </script>
+
